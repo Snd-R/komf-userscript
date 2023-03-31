6228f2c1463b5c59ee4c1d67eec73c9773fee6a5
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-03-31 17:32:21 +0300

    add update urls

--- komf.meta.js
+++ komf.meta.js
@@ -9,5 +9,7 @@
 // @include      http://localhost:5000/*
 // @grant        none
 // @run-at       document-start
+// @updateURL    https://github.com/Snd-R/komf-userscript/blob/main/komf.meta.js
+// @downloadURL  https://github.com/Snd-R/komf-userscript/releases/latest/download/komf.user.js
 
 // ==/UserScript==
