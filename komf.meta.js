da4b5e40025ae6a5ac029ee7b13890e26713a9b9
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-05-12 00:15:01 +0300

    update meta file

--- komf.meta.js
+++ komf.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Komf
 // @namespace    https://github.com/Snd-R/komf-userscript
-// @version      0.10.1
+// @version      0.10.2
 // @description  Komf userscript
 // @author       snd-r
 // @include      http://localhost:8080/*
