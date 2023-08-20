b5b78ab955610562b5eb700dda0c9e70140cfc4b
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-08-20 16:08:09 +0300

    backwards compatibility with Kavita current stable release

--- src/KavitaView.vue
+++ src/KavitaView.vue
@@ -68,11 +68,17 @@ const observer = new window.MutationObserver((mutations) => {
                 }
 
                 let buttons = Array.from(element.getElementsByTagName('button'))
-                let editSeriesButton = buttons.find(elem => elem.getAttribute('id') == 'edit-btn--komf')
+                let editSeriesButton = buttons.find(elem => elem.getAttribute('id') == 'edit-btn--komf' ||
+                  // TODO remove in next kavita stable release after 0.7.7
+                  elem.firstElementChild?.firstElementChild?.classList?.contains('fa-pen') && elem.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.nodeName == 'APP-SERIES-DETAIL'
+                )
                 if (editSeriesButton) {
                     editSeriesButton.parentElement?.insertAdjacentElement('afterend', seriesActionsElement.value)
                 }
-                let libraryFilterButton = buttons.find(elem => elem.getAttribute('id') == 'filter-btn--komf')
+              let libraryFilterButton = buttons.find(elem => elem.getAttribute('id') == 'filter-btn--komf' ||
+              // TODO remove in next kavita stable release after 0.7.7
+                 elem.firstElementChild?.classList.contains('fa-filter') && elem.parentNode?.parentNode?.parentNode?.parentNode?.nodeName == 'APP-LIBRARY-DETAIL'
+            )
                 if (libraryFilterButton && libraryFilterButton.parentElement) {
                     libraryActionsElement.value = libraryFilterButton.parentElement
                 }
